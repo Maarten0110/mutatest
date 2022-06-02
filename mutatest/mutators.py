f44a2e0904d406fa980a12626dce9cab30596c19
Maarten van Tartwijk <m.j.vantartwijk@student.tudelft.nl>
2022-06-02 15:16:32 +0200

    Setup project for publishing.

--- mutatest/mutators.py
+++ mutatest/mutators.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import List
-from replacement_mutator import mutate_by_replacement
+from .replacement_mutator import mutate_by_replacement
 
 
 class Mutator(ABC):
